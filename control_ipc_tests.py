cb556713819b59c3ff51d46f7eba6b13b0fbf93a
Tom Manning <tom@manningetal.com>
2021-01-24 07:06:14 -0500

    change routines to wildcard import

--- control_ipc_tests.py
+++ control_ipc_tests.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-import ctrl_messaging_routines as cipc
-# from ctrl_messaging_routines import *
+from ctrl_messaging_routines import *
 import message_pb2 as mpb
 import logging
 import sys
@@ -17,23 +16,23 @@ logging.basicConfig(
     # filename=('debug.log'),
 )
 
-# logging.info("status: {}".format(cipc.send_msg(cipc.GET, cipc.STATUS)))
+# logging.info("status: {}".format(send_msg(GET, STATUS)))
 
 mode_default = {'mode': 1, 'drill_workout_id': 0, 'drill_step': 0} #, 'iterations': 0}
 params_default = {'level': 2, 'speed': 100, 'height': 0, 'delay': 0}
 
 def run_tests():
-   print("Game test result: {}\n".format(test_game(doubles=False, tie_breaker=True)))
+   # print("Game test result: {}\n".format(test_game(doubles=False, tie_breaker=True)))
    # print("Game test result: {}\n".format(test_game(doubles=True, tie_breaker=False)))
    print("Drill test result: {}\n".format(test_drill(drill_id=39)))
-   # print("Mode test result: {}\n".format(test_register(cipc.MODE, mode_default)))
-   # print("Params test result: {}\n".format(test_register(cipc.PARAMS, params_default)))
+   # print("Mode test result: {}\n".format(test_register(MODE, mode_default)))
+   # print("Params test result: {}\n".format(test_register(PARAMS, params_default)))
 
 
 def test_game(doubles=False, tie_breaker=False):
    # sets mode, params; starts game; stops game
    test_rc = True
-   active = cipc.is_active()
+   active = is_active()
    if (type(active) is not bool):
       return False
    if active:
@@ -44,17 +43,17 @@ def test_game(doubles=False, tie_breaker=False):
    int_tiebreaker = int(tie_breaker == True)
 
    mode = {'mode': mpb.GAME, 'doubles': int_doubles, 'tie_breaker': int_tiebreaker}
-   rc, code = cipc.send_msg(cipc.PUT, cipc.MODE, mode)
+   rc, code = send_msg(PUT, MODE, mode)
    if not rc:
       logging.error("test_game: initial PUT mode failed, code: {}".format(code))
       return False
 
    # start and check for active
-   rc, code = cipc.send_msg(cipc.PUT, cipc.START)
+   rc, code = send_msg(PUT, START)
    if not rc:
       logging.error("test_game: PUT START failed, code: {}".format(code))
       return False
-   active = cipc.is_active()
+   active = is_active()
    if (type(active) is not bool):
       return False
    if not active:
@@ -62,11 +61,11 @@ def test_game(doubles=False, tie_breaker=False):
       test_rc = False
 
    # stop and check for inactive
-   rc, code = cipc.send_msg(cipc.PUT, cipc.STOP)
+   rc, code = send_msg(PUT, STOP)
    if not rc:
       logging.error("test_game: PUT STOP failed, code: {}".format(code))
       return False
-   active = cipc.is_active()
+   active = is_active()
    if (type(active) is not bool):
       return False
    if active:
@@ -74,7 +73,7 @@ def test_game(doubles=False, tie_breaker=False):
       test_rc = False
 
    # check mode for doubles, tieb_break
-   rc, register = cipc.send_msg(cipc.GET, cipc.MODE)
+   rc, register = send_msg(GET, MODE)
    if not rc:
       logging.error("test_game: final GET mode failed")
       return False
@@ -96,7 +95,7 @@ def test_game(doubles=False, tie_breaker=False):
 def test_drill(drill_id=39):
    # sets mode, params; starts drill; stops drill
    test_rc = True
-   active = cipc.is_active()
+   active = is_active()
    if (type(active) is not bool):
       return False
    if active:
@@ -104,17 +103,17 @@ def test_drill(drill_id=39):
       return False
 
    mode = {'mode': mpb.DRILL, 'drill_workout_id': drill_id}
-   rc, code = cipc.send_msg(cipc.PUT, cipc.MODE, mode)
+   rc, code = send_msg(PUT, MODE, mode)
    if not rc:
       logging.error("test_drill: initial PUT mode failed, code: {}".format(code))
       return False
 
    # start and check for active
-   rc, code = cipc.send_msg(cipc.PUT, cipc.START)
+   rc, code = send_msg(PUT, START)
    if not rc:
       logging.error("test_drill: PUT START failed, code: {}".format(code))
       return False
-   active = cipc.is_active()
+   active = is_active()
    if (type(active) is not bool):
       return False
    if not active:
@@ -122,11 +121,11 @@ def test_drill(drill_id=39):
       test_rc = False
 
    # stop and check for inactive
-   rc, code = cipc.send_msg(cipc.PUT, cipc.STOP)
+   rc, code = send_msg(PUT, STOP)
    if not rc:
       logging.error("test_game: PUT STOP failed, code: {}".format(code))
       return False
-   active = cipc.is_active()
+   active = is_active()
    if (type(active) is not bool):
       return False
    if active:
@@ -140,7 +139,7 @@ def test_drill(drill_id=39):
 def test_register(register_under_test, default_values):
     test_pattern = {}
     read_back_compare = True
-    msg_ok, register_original = cipc.send_msg(cipc.GET, register_under_test)
+    msg_ok, register_original = send_msg(GET, register_under_test)
     if not msg_ok:
         logging.error("GET {} failed.".format(register_under_test))
         return False
@@ -157,10 +156,10 @@ def test_register(register_under_test, default_values):
                 test_pattern[key] = 2
             j += 1
         logging.debug("Setting {} to: {}".format(register_under_test, test_pattern))
-        if cipc.send_msg(cipc.PUT, register_under_test, test_pattern) == False:
+        if send_msg(PUT, register_under_test, test_pattern) == False:
             logging.error("PUT {} failed".format(register_under_test))
             break
-        msg_ok, reg_read_back = cipc.send_msg(cipc.GET, register_under_test)
+        msg_ok, reg_read_back = send_msg(GET, register_under_test)
         if not msg_ok:
             logging.error("GET {} failed.".format(register_under_test))
             return False
@@ -172,9 +171,9 @@ def test_register(register_under_test, default_values):
         if not read_back_compare:
             break
     #restore 
-    if (cipc.send_msg(cipc.PUT, register_under_test, register_original) == False):
+    if (send_msg(PUT, register_under_test, register_original) == False):
         logging.error("restore {} failed".format(register_under_test))
-    logging.info("Register test done: {} restored to: {}".format(register_under_test, cipc.get(register_under_test)))
+    logging.info("Register test done: {} restored to: {}".format(register_under_test, register_original))
     return(read_back_compare)
 
 if __name__ == "__main__":
--- ctrl_messaging_routines.py
+++ ctrl_messaging_routines.py
@@ -1,15 +1,17 @@
 
 __all__ = ["send_msg", "is_active", "GET", "PUT", "STATUS", "MODE", "PARAMS", "START", "STOP"]
 
-import os
-import sys
-import errno
+import os as _os
+from sys import platform as _platform
+import errno as _errno
+from select import select as _select
 import logging
-import select
 
 from protobuf_to_dict import protobuf_to_dict, dict_to_protobuf
 
-from message_pb2 import b_status_msg, b_mode_msg, b_params_msg
+from message_pb2 import b_status_msg as _b_status_msg
+from message_pb2 import b_mode_msg as _b_mode_msg
+from message_pb2 import b_params_msg as _b_params_msg
 
 _logger = logging.getLogger(__name__)
 # log_format = ('[%(asctime)s] %(levelname)-6s %(name)-12s %(message)s')
@@ -34,7 +36,7 @@ STOP = "STOP_"
 
 _OK = bytearray("BIPC 200 ", 'utf-8')
 
-if 'darwin' in sys.platform:
+if 'darwin' in _platform:
    _FIFO_BASE_TO_CTRL = "/tmp/BaseToCtrl.fifo"
    _FIFO_CTRL_TO_BASE = "/tmp/CtrlToBase.fifo"
 else:
@@ -51,20 +53,20 @@ def init(read_fifo=_FIFO_BASE_TO_CTRL, write_fifo=_FIFO_CTRL_TO_BASE):
    global _fd_write
 
    try:
-      os.mkfifo(write_fifo)
-      os.mkfifo(read_fifo)
+      _os.mkfifo(write_fifo)
+      _os.mkfifo(read_fifo)
    except OSError as oe:
-      if oe.errno != errno.EEXIST:
+      if oe.errno != _errno.EEXIST:
          raise
 
    # logging.debug('Opening {} for writing.'.format(write_fifo))
    # the following does NOT block until read end is open
-   _fd_write = os.open(write_fifo, os.O_WRONLY)
+   _fd_write = _os.open(write_fifo, _os.O_WRONLY)
    logging.debug("{} opened.".format(write_fifo))
 
    # logging.debug('Opening {} for reading.'.format(read_fifo))
    # the following does! block until write end is open
-   _fd_read = os.open(read_fifo, os.O_RDONLY)
+   _fd_read = _os.open(read_fifo, _os.O_RDONLY)
    logging.debug("{} opened.".format(read_fifo))
 
 
@@ -75,10 +77,10 @@ def empty_pipe():
       init()
    pipe_empty = False
    while not pipe_empty:
-      fd_r, _, _ = select.select([_fd_read], [], [], 0)
+      fd_r, _, _ = _select([_fd_read], [], [], 0)
       if _fd_read in fd_r:
             print("read pipe before sending msg: {}".format(
-               os.read(_fd_read, _MAX_BYTES_TO_READ)))
+               _os.read(_fd_read, _MAX_BYTES_TO_READ)))
             _non_empty_pipe_count += 1
       else:
             pipe_empty = True
@@ -95,39 +97,36 @@ def send_msg(method=GET, resource=STATUS, settings={}):
    rcvd_encoded_msg = None
    send_encoded_msg = None
    if (method is GET):
-      header_bytes = str.encode("BIPC GET " + resource + " ")
+      header_bytes = str.encode("BIPC " + GET + " " + resource + " ")
       if (resource is STATUS):
-         rcvd_encoded_msg = b_status_msg()
+         rcvd_encoded_msg = _b_status_msg()
       elif (resource is MODE):
-         rcvd_encoded_msg = b_mode_msg()
+         rcvd_encoded_msg = _b_mode_msg()
       elif (resource is PARAMS):
-         rcvd_encoded_msg = b_params_msg()
+         rcvd_encoded_msg = _b_params_msg()
       else:
-         logging.error("GET {} not supported".format(resource))
+         logging.error("{} {} not supported".format(GET, resource))
    elif (method is PUT):
-      # if len(settings) == 0:
-      #     logging.error("No data specified on PUT {}".format(resource))
-      # else:
       header_bytes = str.encode("BIPC PUT " + resource + " ")
       if (resource is MODE):
          send_encoded_msg = dict_to_protobuf(
-               b_mode_msg, values=settings).SerializeToString()
+               _b_mode_msg, values=settings).SerializeToString()
       elif (resource is PARAMS):
          send_encoded_msg = dict_to_protobuf(
-               b_params_msg, values=settings).SerializeToString()
+               _b_params_msg, values=settings).SerializeToString()
 
    if (rcvd_encoded_msg is not None or method is PUT):
       if (send_encoded_msg is not None):
-         bytes_written = os.write(_fd_write, header_bytes + send_encoded_msg)
+         bytes_written = _os.write(_fd_write, header_bytes + send_encoded_msg)
       else:
-         bytes_written = os.write(_fd_write, header_bytes)
+         bytes_written = _os.write(_fd_write, header_bytes)
 
       if bytes_written < 1:
          logging.debug("FIFO write failed; read end closed?")
       else:
          logging.debug("Wrote {}".format(header_bytes))
 
-      data = os.read(_fd_read, _MAX_BYTES_TO_READ)
+      data = _os.read(_fd_read, _MAX_BYTES_TO_READ)
       if len(data) == 0:
          logging.debug("FIFO read failed; write end closed?")
       else:
@@ -148,7 +147,6 @@ def send_msg(method=GET, resource=STATUS, settings={}):
                logging.error(
                   "{} {} rejected - code: {}".format(method, resource, code))
                
-
    if (method is PUT):
       return rc, code
    else:
@@ -165,94 +163,3 @@ def is_active():
          return True
       else:
          return False
-
-
-'''
-def get(type=STATUS):
-    global _fd_read
-    global _fd_write
-    rc = False
-    if _fd_write is None:
-        init()
-
-    rcvd_encoded_msg = None
-    if (type is STATUS):
-        rcvd_encoded_msg = b_status_msg()
-    elif (type is MODE):
-        rcvd_encoded_msg = b_mode_msg()
-    elif (type is PARAMS):
-        rcvd_encoded_msg = b_params_msg()
-    else:
-        logging.error("GET {} not supported".format(type))
-
-    if (rcvd_encoded_msg is not None):
-        out_header = str.encode("BIPC GET " + type + " ")
-        bytes_written = os.write(_fd_write, out_header)
-        if bytes_written < 1:
-            logging.debug("FIFO write failed; read end closed?")
-        else:
-            logging.debug("Wrote {}".format(out_header))
-
-        data = os.read(_fd_read, _MAX_BYTES_TO_READ)
-        if len(data) == 0:
-            logging.debug("FIFO read failed; write end closed?")
-        else:
-            logging.debug("Read: Header: {}   Body: {}".format(
-                data[0:14], data[_BIPC_HEADER_LENGTH:]))
-            if data.startswith(_OK):
-                bytes_parsed = rcvd_encoded_msg.ParseFromString(
-                    data[_BIPC_HEADER_LENGTH:])
-                if (bytes_parsed != len(data)-_BIPC_HEADER_LENGTH):
-                    logging.error("Message parse error: parsed: {} - should be {}".
-                                  format(bytes_parsed, len(data)-_BIPC_HEADER_LENGTH))
-                else:
-                    rc = protobuf_to_dict(rcvd_encoded_msg)
-            else:
-                logging.error("Get rejected. code: {}".format(data[5:8]))
-
-    return rc
-
-
-def set(type=MODE, settings={'mode': 1}):
-    global _fd_read
-    global _fd_write
-    rc = False
-
-    if _fd_write is None:
-        init()
-
-    # mode_msg = b_mode_msg()
-    # mode_msg.mode = mode
-
-    empty_pipe()
-
-    msg_bytes = None
-    if (type is MODE):
-        msg_bytes = dict_to_protobuf(
-            b_mode_msg, values=settings).SerializeToString()
-    elif (type is PARAMS):
-        msg_bytes = dict_to_protobuf(
-            b_params_msg, values=settings).SerializeToString()
-    else:
-        logging.error("PUT {} not supported".format(type))
-
-    if (msg_bytes is not None):
-        header_bytes = str.encode("BIPC PUT " + type + " ")
-        bytes_written = os.write(_fd_write, header_bytes + msg_bytes)
-        if bytes_written < 1:
-            logging.debug("FIFO write failed; read end closed?")
-        else:
-            logging.debug("Wrote {}".format(header_bytes + msg_bytes))
-
-        data = os.read(_fd_read, _MAX_BYTES_TO_READ)
-        # logging.debug("Read: Header: {}   Body: {}".format(data[0:14], data[_BIPC_HEADER_LENGTH:]))
-        if len(data) == 0:
-            logging.debug("FIFO read failed; write end closed?")
-        else:
-            if data.startswith(_OK):
-                rc = True
-            else:
-                logging.error("SET MODE rejected. code: {}".format(data[5:8]))
-
-    return rc
-'''

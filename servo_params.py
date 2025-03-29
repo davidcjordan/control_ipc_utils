77fc7c05a9cbf5f646282c7c1ba0fd837d9192c0
manningt <tom@manningetal.com>
2025-03-29 07:41:22 -0400

    test angle parameters

--- servo_params.py
+++ servo_params.py
@@ -13,6 +13,8 @@ if __name__ == '__main__':
    from ctrl_messaging_routines import send_msg
    from control_ipc_defines import GET_METHOD, PUT_METHOD, CTRL_TRANSPRT, SRVO_RSRC
    from control_ipc_defines import CENTER_ANGLE_PARAM, DROP_SPEED_PARAM, LOB_SPEED_PARAM
+   from control_ipc_defines import SERVE_ANGLE_PARAM, FLAT_ANGLE_PARAM, LOOP_ANGLE_PARAM
+   from control_ipc_defines import CHIP_ANGLE_PARAM, TOPSPIN_ANGLE_PARAM, PASS_ANGLE_PARAM
 
    msg_ok, servo_params = send_msg(GET_METHOD, SRVO_RSRC, channel=CTRL_TRANSPRT)
    if not msg_ok:
@@ -20,7 +22,12 @@ if __name__ == '__main__':
    else:
       print(f"servo parameters: {servo_params}")
 
-   params = {CENTER_ANGLE_PARAM: 11, DROP_SPEED_PARAM: 22, LOB_SPEED_PARAM: 33}
+   params = {CENTER_ANGLE_PARAM: 25, DROP_SPEED_PARAM: 284, LOB_SPEED_PARAM: 412}
+   ang_params1 = {SERVE_ANGLE_PARAM: 219, FLAT_ANGLE_PARAM: 135, LOOP_ANGLE_PARAM: 145}
+   ang_params2 = {CHIP_ANGLE_PARAM: 199, TOPSPIN_ANGLE_PARAM: 250, PASS_ANGLE_PARAM: 215}
+   params.update(ang_params1)
+   params.update(ang_params2)
+   logging.debug(f"params: {params}")
    rc, code = send_msg(PUT_METHOD, SRVO_RSRC, params, channel=CTRL_TRANSPRT)
    if not rc:
       logging.error("PUT servo_values failed, code: {}".format(code))
